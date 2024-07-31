bcb3c2f4fa6bfc5cb178c15a2ba10470a825342a
Abdurahmon2004 <karimjonovabdurahmon70@gmail.com>
2024-07-31 23:16:11 +0500

    'sdsdsd'

--- database/migrations/2024_07_31_053937_create_orders_table.php
+++ database/migrations/2024_07_31_053937_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id')->nullable();
+            $table->string('phone')->nullable();
             $table->string('quantity')->nullable();
             $table->string('location')->nullable();
             $table->boolean('status')->nullable();
