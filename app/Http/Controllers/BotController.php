89f449ae75dc1ef05563d607fd04908387cf18b7
Abdurahmon2004 <karimjonovabdurahmon70@gmail.com>
2024-07-31 09:41:09 +0500

    'sdsd'

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -18,7 +18,7 @@ class BotController extends Controller
             $data = $update['callback_query']['data'] ?? null;
             $messageId = $update['message']['message_id'] ?? $update['callback_query']['message']['message_id'] ?? null;
             $contact = $update['message']['contact'] ?? null;
-
+            \Log::info($data);
             if ($chatId && $text) {
                 $this->handleMessage($chatId, $text, $messageId);
             }
