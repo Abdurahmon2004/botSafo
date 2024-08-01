99a7173030dd3f7922c044736b6a93e3f00be5a6
Abdurahmon2004 <karimjonovabdurahmon70@gmail.com>
2024-08-01 10:46:54 +0500

    'sdds'

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -17,7 +17,10 @@ class BotController extends Controller
             $chatId = $update['message']['chat']['id'] ?? $update['callback_query']['message']['chat']['id'] ?? null;
             $name = $update['message']['chat']['first_name'] ?? $update['callback_query']['message']['chat']['first_name'] ?? null;
             $text = $update['message']['text'] ?? null;
-            \Log::info('Kiritilgan qiymat: ' . $text);
+            if($text == 0){
+                $text = 1;
+            }
+            \Log::info('Kiritilgan miqdori:'.$text);
             $data = $update['callback_query']['data'] ?? null;
             $messageId = $update['message']['message_id'] ?? $update['callback_query']['message']['message_id'] ?? null;
             $contact = $update['message']['contact'] ?? null;
